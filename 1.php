660c999202bfdc8dcef43fef6560e85ce8c10965
lilinlong1224 <2811778708@qq.com>
2018-01-15 02:17:23 +0800

    gittest

---
+++ 1.php
@@ -0,0 +1,16 @@
+<?php
+
+function foo()
+{
+    $numargs = func_num_args(); //参数数量
+    echo "参数个数是:{$numargs}<br />\n";
+    if ($numargs >= 2) {
+        echo "第二个参数的值:" . func_get_arg(1) . "<br />\n";
+    }
+    $arg_list = func_get_args();
+    for ($i = 0; $i < $numargs; $i++) {
+        echo "第{$i}个参数值:{$arg_list[$i]}<br />\n";
+    }
+}
+ 
+foo(1, 'd', 3,4);
